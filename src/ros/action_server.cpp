63f33e1339b44e9a6d017ad1c6beda0d15c0ca38
dennis <dennis@bluehill.coffee>
2021-02-27 23:59:04 -0800

    now use sharpness for moveJ

--- src/ros/action_server.cpp
+++ src/ros/action_server.cpp
@@ -324,17 +324,23 @@ void ActionServer::trajectoryThread() {
 
     // Modified frame_id parameter pass through
     std::stringstream ss(goal->trajectory.header.frame_id);
-    std::string move_type; uint64_t goal_id; double sharpness; double servoj_gain; double servoj_lookahead_time;
-    ss >> goal_id >> sharpness >> move_type;
+    uint64_t goal_id; double sharpness; double servoj_gain; double servoj_lookahead_time;
+    ss >> goal_id >> sharpness;
+    bool moveJ = false;
+    if (sharpness < 0)
+    {
+      moveJ = true;
+      sharpness = 0;
+    }
     // convex combination between (100, 0.2) -> (500, 0.05)
     // FIXME configure as rosparam
     servoj_gain = (1 - sharpness) * 100 + sharpness * 500;
     servoj_lookahead_time = (1 - sharpness) * 0.2 + sharpness * 0.05;
     LOG_INFO("Received trajectory parameters goal_id: %d, gain: %f, lookahead_time: %f", (int)goal_id, servoj_gain, servoj_lookahead_time);
-    LOG_INFO("Attempting to start follower %p, move_type: %s", &follower_, move_type.c_str());
+    LOG_INFO("Attempting to start follower %p, moveJ? %d", &follower_, int(moveJ));
     if (follower_.start(servoj_gain, servoj_lookahead_time)) {
       follower_.current_gh_id = std::to_string(goal_id);
-      if (move_type == "moveJ" ?
+      if (moveJ ?
           follower_.execute_moveJ(trajectory, interrupt_traj_, pause_traj_) :
           follower_.execute(trajectory, interrupt_traj_, pause_traj_)
           ) {
